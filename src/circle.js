cf85ce99874035db732f92addcd2e09050cd37c6
NathanCotrim <nathan.cotrim@gmail.com>
2021-11-24 12:01:15 -0300

    feat: add class circle

--- src/circle.js
+++ src/circle.js
@@ -1,1 +1,27 @@
-console.log('circle');
+
+
+export class Circle {
+    constructor(positions = { x: 0, y: 0 }, ray = 1, color = '#ffffff') {
+        this.positions = positions 
+        this.ray = ray
+        this.color = color
+    }
+
+    setPosition(positions = this.positions) {
+        this.positions = positions
+    }
+
+    draw(canvasContext) {
+        canvasContext.strokeStyle = this.color
+
+        canvasContext.beginPath()
+        canvasContext.arc(
+            this.positions.x,
+            this.positions.y,
+            this.ray,
+            0,
+            Math.PI * 2
+        )
+        canvasContext.stroke()
+    }
+}

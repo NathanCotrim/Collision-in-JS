30195797fe82f6327a99365c4a10f483f11bbc98
NathanCotrim <nathan.cotrim@gmail.com>
2021-11-27 16:12:34 -0300

    feat: setDimensions

--- src/circle.js
+++ src/circle.js
@@ -1,14 +1,32 @@
-
+const RADIUS_RANGE = {
+    min: 5,
+    max: 50
+}
 
 export class Circle {
-    constructor(positions = { x: 0, y: 0 }, ray = 1, color = '#ffffff') {
+    constructor(positions = { x: 0, y: 0 }, radius = 1, color = '#ffffff') {
         this.positions = positions 
-        this.ray = ray
+        this.radius = radius
         this.color = color
     }
 
     setPosition(positions = this.positions) {
         this.positions = positions
+    }
+
+    setRadius(radius) {
+        this.radius = radius
+    }
+
+    setDimensions(screenWidth, screenHeight) {
+        this.setPosition({
+            x: Math.random() * screenWidth,
+            y: Math.random() * screenHeight
+        })
+
+        const randomRadius = Math.random() * (RADIUS_RANGE.max - RADIUS_RANGE.min) + RADIUS_RANGE.min
+
+        this.setRadius(randomRadius);
     }
 
     draw(canvasContext) {
@@ -18,7 +36,7 @@ export class Circle {
         canvasContext.arc(
             this.positions.x,
             this.positions.y,
-            this.ray,
+            this.radius,
             0,
             Math.PI * 2
         )

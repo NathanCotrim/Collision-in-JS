4f589b6fd250501db272e89e659d79246bd314cd
NathanCotrim <nathan.cotrim@gmail.com>
2021-11-28 11:29:01 -0300

    feat: set overlap state

--- src/circle.js
+++ src/circle.js
@@ -1,45 +1,63 @@
 const RADIUS_RANGE = {
     min: 5,
     max: 50
-}
+};
+
+const OVERLAPING_COLOR = '#ff0000'
 
 export class Circle {
     constructor(positions = { x: 0, y: 0 }, radius = 1, color = '#ffffff') {
-        this.positions = positions 
-        this.radius = radius
-        this.color = color
+        this.positions = positions;
+        this.radius = radius;
+        this.color = color;
+        this.overlaping = false;
     }
 
     setPosition(positions = this.positions) {
-        this.positions = positions
+        this.positions = positions;
     }
 
     setRadius(radius) {
-        this.radius = radius
+        this.radius = radius;
     }
 
     setDimensions(screenWidth, screenHeight) {
         this.setPosition({
             x: Math.random() * screenWidth,
             y: Math.random() * screenHeight
-        })
+        });
 
-        const randomRadius = Math.random() * (RADIUS_RANGE.max - RADIUS_RANGE.min) + RADIUS_RANGE.min
+        const randomRadius = Math.random() * (RADIUS_RANGE.max - RADIUS_RANGE.min) + RADIUS_RANGE.min;
 
         this.setRadius(randomRadius);
     }
 
-    draw(canvasContext) {
-        canvasContext.strokeStyle = this.color
+    isOverlaped(circle) {
+        const dx = circle.positions.x - this.positions.x;
+        const dy = circle.positions.y - this.positions.y;
 
-        canvasContext.beginPath()
+        return Math.sqrt(dx * dx + dy * dy) <= this.radius + circle.radius;
+    }
+
+    setOverlaping(boolean) {
+        this.overlaping = boolean
+    }
+
+    updateState() {
+        this.overlaping = false
+    }
+
+    draw(canvasContext) {
+        canvasContext.strokeStyle = this.overlaping ? OVERLAPING_COLOR : this.color;
+
+        canvasContext.beginPath();
         canvasContext.arc(
             this.positions.x,
             this.positions.y,
             this.radius,
             0,
             Math.PI * 2
-        )
-        canvasContext.stroke()
+        );
+        canvasContext.stroke();
     }
 }
